a833e243c5f9c5f5389b0b9eec3d7d006f897518
dev <keep.it.simple.dev@gmail.com>
2025-09-29 00:35:17 -0400

    initial implementation

--- app/src/main/java/org/keepitsimple/draftlottery/TieredPowerball.java
+++ app/src/main/java/org/keepitsimple/draftlottery/TieredPowerball.java
@@ -1,21 +1,51 @@
 package org.keepitsimple.draftlottery;
 
 import static java.lang.Math.pow;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TieredPowerball {
 
     public static String[] determineDraftOrder(String[] teamsRankedFromLowestChance) {
-        //https://www.bigocheatsheet.com/
+        List<TeamChances> teamsChances = calculateTeamChances(teamsRankedFromLowestChance);
 
-        TeamChances[] teamsChances = calculateTeamChances(teamsRankedFromLowestChance);
+        // can upgrade this to stream in Java 21
+        int total = 0;
+        for (TeamChances teamChances : teamsChances) {
+            total += teamChances.getChancesCount();
+        }
 
-        return null;
+        String[] draftOrder = new String[teamsChances.size()];
+
+        for (int draftPosition = 0; !teamsChances.isEmpty(); draftPosition++) {
+            long result = Math.round(Math.random() * total);
+
+            boolean picked = false;
+            int current = 0;
+            for (int i = 0; i < teamsChances.size(); i++) {
+                current += teamsChances.get(i).getChancesCount();
+
+                if (result <= current) {
+                    draftOrder[draftPosition] = teamsChances.get(i).getTeam();
+                    teamsChances.remove(i);
+                    picked = true;
+                    break;
+                }
+            }
+            if (!picked) {
+                // improve handling here - maybe make new exception
+                throw new RuntimeException("not picked");
+            }
+        }
+
+        return draftOrder;
     }
 
-    public static TeamChances[] calculateTeamChances(String[] teamsRankedFromLowestChance) {
-        TeamChances[] teamsChances = new TeamChances[teamsRankedFromLowestChance.length];
+    public static List<TeamChances> calculateTeamChances(String[] teamsRankedFromLowestChance) {
+        List<TeamChances> teamsChances = new LinkedList<>();
         for (int i = 0; i < teamsRankedFromLowestChance.length; i++) {
-            teamsChances[i] = new TeamChances(teamsRankedFromLowestChance[i], (int)Math.round(pow(2d, Double.valueOf(i))));
+            // TODO: make this math less ugly
+            teamsChances.add(new TeamChances(teamsRankedFromLowestChance[i], (int)Math.round(pow(2d, Double.valueOf(i)))));
         }
         return teamsChances;
     }
--- app/src/test/java/org/keepitsimple/draftlottery/TieredPowerballTest.java
+++ app/src/test/java/org/keepitsimple/draftlottery/TieredPowerballTest.java
@@ -1,39 +1,70 @@
 package org.keepitsimple.draftlottery;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
 
 public class TieredPowerballTest {
 
+    // TODO: consider parameterizing these three tests
     @Test
-    public void TieredPowerballTest() {
+    public void determineDraftOrderOneTeamTest() {
+        String[] teams = {"1st"};
+        String[] result = TieredPowerball.determineDraftOrder(teams);
+
+        assertThat(result.length, is(teams.length));
+        assertThat(result[0], is("1st"));
+    }
+
+    @Test
+    public void determineDraftOrderTwoTeamsTest() {
+        String[] teams = {"1st", "2nd"};
+        String[] result = TieredPowerball.determineDraftOrder(teams);
+
+        assertThat(result.length, is(teams.length));
+        assertThat(List.of(result), containsInAnyOrder(teams));
+    }
+
+    @Test
+    public void determineDraftOrderThreeTeamsTest() {
+        String[] teams = {"1st", "2nd", "3rd"};
+        String[] result = TieredPowerball.determineDraftOrder(teams);
+
+        assertThat(result.length, is(teams.length));
+        assertThat(List.of(result), containsInAnyOrder(teams));
+    }
+
+    @Test
+    public void calculateTeamChancesTest() {
         String[] teamsRankedFromLowestChance = {"1st", "2nd", "4th", "3rd", "8th", "7th", "6th", "5th"};
 
-        TeamChances[] teamsChances = TieredPowerball.calculateTeamChances(teamsRankedFromLowestChance);
+        List<TeamChances> teamsChances = TieredPowerball.calculateTeamChances(teamsRankedFromLowestChance);
 
-        assertThat(teamsChances[0].getTeam(), is("1st"));
-        assertThat(teamsChances[0].getChancesCount(), is(1));
+        assertThat(teamsChances.get(0).getTeam(), is("1st"));
+        assertThat(teamsChances.get(0).getChancesCount(), is(1));
 
-        assertThat(teamsChances[1].getTeam(), is("2nd"));
-        assertThat(teamsChances[1].getChancesCount(), is(2));
+        assertThat(teamsChances.get(1).getTeam(), is("2nd"));
+        assertThat(teamsChances.get(1).getChancesCount(), is(2));
 
-        assertThat(teamsChances[2].getTeam(), is("4th"));
-        assertThat(teamsChances[2].getChancesCount(), is(4));
+        assertThat(teamsChances.get(2).getTeam(), is("4th"));
+        assertThat(teamsChances.get(2).getChancesCount(), is(4));
 
-        assertThat(teamsChances[3].getTeam(), is("3rd"));
-        assertThat(teamsChances[3].getChancesCount(), is(8));
+        assertThat(teamsChances.get(3).getTeam(), is("3rd"));
+        assertThat(teamsChances.get(3).getChancesCount(), is(8));
 
-        assertThat(teamsChances[4].getTeam(), is("8th"));
-        assertThat(teamsChances[4].getChancesCount(), is(16));
+        assertThat(teamsChances.get(4).getTeam(), is("8th"));
+        assertThat(teamsChances.get(4).getChancesCount(), is(16));
 
-        assertThat(teamsChances[5].getTeam(), is("7th"));
-        assertThat(teamsChances[5].getChancesCount(), is(32));
+        assertThat(teamsChances.get(5).getTeam(), is("7th"));
+        assertThat(teamsChances.get(5).getChancesCount(), is(32));
 
-        assertThat(teamsChances[6].getTeam(), is("6th"));
-        assertThat(teamsChances[6].getChancesCount(), is(64));
+        assertThat(teamsChances.get(6).getTeam(), is("6th"));
+        assertThat(teamsChances.get(6).getChancesCount(), is(64));
 
-        assertThat(teamsChances[7].getTeam(), is("5th"));
-        assertThat(teamsChances[7].getChancesCount(), is(128));
+        assertThat(teamsChances.get(7).getTeam(), is("5th"));
+        assertThat(teamsChances.get(7).getChancesCount(), is(128));
     }
 }
